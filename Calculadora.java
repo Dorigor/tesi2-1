165384354f9261039fc39eb7ba64d2f92df32a49
Dorigor <nramosrodrigo@gmail.com>
2016-01-20 17:57:31 -0200

    Calculadora com Classe Scanner
    Agora melhorou.

--- Calculadora.java
+++ Calculadora.java
@@ -1,9 +1,17 @@
-public class Calculadora{
+import java.util.Scanner;
+
+public Class Calculadora{
   public class void main(String[] args){
   
   int a, b;
-  a=3;
-  b=2;
-    System.out.println(a+b);
+  
+  System.out.println("Entre com o primeiro número: ");
+  Scanner pegador = new Scanner(System.in);
+  a = pegador.NextInt();
+  
+  System.out.println("Entre com o segundo número: ");
+  b = pegador.NextInt();
+  
+  return System.out.println("A soma dos dois números é: " (a+b);
   }
 }
